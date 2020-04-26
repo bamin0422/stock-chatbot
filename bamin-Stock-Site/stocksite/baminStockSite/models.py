edfb8f0f143f39213ec5e77fcea3878e810b9eba
bamin0422 <bamin0422@gmail.com>
2020-04-26 18:21:16 +0900

    add pages

--- bamin-Stock-Site/stocksite/baminStockSite/models.py
+++ bamin-Stock-Site/stocksite/baminStockSite/models.py
@@ -34,6 +34,16 @@ class StockCommunity(Post):
         return self.title
 
 
+class StockCommunity(Post):
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
 class StockNews(Post):
 
     def publish(self):
--- bamin-Stock-Site/stocksite/baminStockSite/urls.py
+++ bamin-Stock-Site/stocksite/baminStockSite/urls.py
@@ -2,7 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.specialStock_list, name='specialStock_list'),
-    path('', views.stockNews_list, name='stockNews_list'),
-    path('', views.stockCommunity_list, name='stockCommunity_list'),
+
+    path('', views.home, name='home'),
+    path('specialstock/', views.specialStock_list, name='specialStock_list'),
+    path('specialstock_detail/', views.specialStock_list_detail,
+         name='specialStock_list_detail'),
+    path('stocknews/', views.stockNews_list, name='stockNews_list'),
+    path('stocknews_detail/', views.stockNews_list_detail,
+         name='stockNews_list_detail'),
+    path('stockcommunity/', views.stockCommunity_list, name='stockCommunity_list'),
+    path('stockcommunity_detail/', views.stockCommunity_list_detail,
+         name='stockCommunity_list_detail'),
+
 ]
